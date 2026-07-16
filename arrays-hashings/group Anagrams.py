c1892825391fee79d77a7c6557b01872f3a0b1b4
samiee <sampathrajana01@gmail.com>
2026-07-16 23:20:11 +0530

    Add files via upload

---
+++ arrays-hashings/group Anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group = {}
+        for i in strs:
+            key = "".join(sorted(i))
+            if key not in group:
+                group[key] = []
+            group[key].append(i)
+        return list(group.values())   
