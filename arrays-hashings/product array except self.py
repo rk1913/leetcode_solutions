c620b30c32e45caebbfcf7418901a4d83d2101eb
samiee <sampathrajana01@gmail.com>
2026-07-17 21:36:38 +0530

    Add files via upload

---
+++ arrays-hashings/product array except self.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        l = [1]*n
+        r = [1]*n
+        a = [1]*n
+        for i in range(1,n):
+            l[i] = l[i-1] * nums[i-1]
+        for i in range(n-2,-1,-1):
+            r[i] = r[i+1] *nums[i+1]
+        for i in range(len(nums)):
+            a[i] = l[i] * r[i]
+
+        return a            
+          
